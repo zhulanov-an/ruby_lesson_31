5ce04fe710276ee06d8ea758d7311f8ca19597d8
Zhulanov Anton <zhulanoff2007@gmail.com>
2016-04-11 00:29:10 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# It's a clean project with libs:
+1. Bootstrap
+2. Datetimepicker
+3. Colorpicker
+4. ActiveRecord
+5. SQlite3
---
+++ app.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, "sqlite3:./database/barbershop.db"
+
+get '/' do
+	erb 'Hello!'
+end
